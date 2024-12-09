598880d36efca5639340465437ad35f0500029fe
Kent4r <53953044+Kent4r@users.noreply.github.com>
2024-12-09 20:07:14 +0300

    base analysis + txt file of obj type columns

--- Analyze.py
+++ Analyze.py
@@ -1,13 +1,19 @@
-# %% Импорт библиотек, чтение и вывод (первые 10 строк) данных
+# %% Импорт библиотек, чтение и вывод фигуры данных
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv('train.csv')
+df.shape
 
-df.head(10)
-
-# %% Удаление пустых колонок и вывод первых 10 строк для наблюдения, что изменилось
+# %% Удаление пустых колонок и смотрим, что изменилось
 df.dropna(how='all', axis=1, inplace=True)
-
-df.head(10)
+df.shape
+# %% смотрим сколько колонок какого типа
+df.info()
+# %% 
+list(df.select_dtypes(['object']).columns)
+# %%
+f = open('list_of_object_col.txt', 'w')
+f.write(f"{str(list(df.select_dtypes(['object']).columns)).replace(', ', '\n').replace("'", '').replace('[','').replace(']', '')}")
+f.close()
 # %%
