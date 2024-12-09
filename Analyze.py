4048f51f255bd4cc54e046900dd960c751c40fe8
Kent4r <53953044+Kent4r@users.noreply.github.com>
2024-12-10 00:39:31 +0300

    some more analysis

--- Analyze.py
+++ Analyze.py
@@ -1,17 +1,32 @@
-# %% Импорт библиотек, чтение и вывод фигуры данных
+# %%
+# Импорт библиотек, чтение и вывод фигуры данных
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv('train.csv')
 df.shape
 
-# %% Удаление пустых колонок и смотрим, что изменилось
+# %%
+# Удаление пустых колонок и смотрим, что изменилось
 df.dropna(how='all', axis=1, inplace=True)
 df.shape
-# %% смотрим сколько колонок какого типа
+# %%
+# смотрим сколько колонок какого типа
 df.info()
-# %% записываем весь список колонок типа obj
-f = open('list_of_object_col.txt', 'w')
+# %%
+# записываем весь список колонок типа obj
+f = open('list_of_object_cols.txt', 'w')
 f.write(f"{str(list(df.select_dtypes(['object']).columns)).replace(', ', '\n').replace("'", '').replace('[','').replace(']', '')}")
+f.close()
+# %%
+# Смотрим все уникальные значения колонок obj
+text = ""
+for col in list(df.select_dtypes(['object']).columns): text += f"{col}:\n{str(df[col].unique())}\n\n\n"
+f = open('list_of_unique_values_in_object_cols.txt', 'w', encoding='utf-8')
+
+dic = {'nan':'',    "' ":'\n',  " '":'',    "'":'',     '[':'',     ']':''}
+for i, j in dic.items(): text = text.replace(i, j)
+
+f.write(text)
 f.close()
 # %%
