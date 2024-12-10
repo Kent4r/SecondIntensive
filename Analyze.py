afd0b1e2c2149cf83bf0bf2552c3aa05a6d63178
Kent4r <egor.bukharov06@gmail.com>
2024-12-10 12:03:05 +0300

    ffff

--- Analyze.py
+++ Analyze.py
@@ -30,3 +30,14 @@ for i, j in dic.items(): text = text.replace(i, j)
 f.write(text)
 f.close()
 # %%
+# Делаем то же самое, но с float64
+text = ""
+for col in list(df.select_dtypes(['float']).columns): text += f"{col}:\n{str(df[col].unique())}\n\n\n"
+f = open('list_of_unique_values_in_float_cols.txt', 'w', encoding='utf-8')
+
+dic = {'nan':'',"' ":'\n'," '":'',"'":'','[':'',']':'','  ':'',}
+for i, j in dic.items(): text = text.replace(i, j)
+
+f.write(text)
+f.close()
+# %%
