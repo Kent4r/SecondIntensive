10df5bdbad0bef12e927002fc026be83ae2cf72a
Ваше Имя <user@comp2-315>
2024-12-11 11:20:21 +0300

    refreshing code/optimizing

--- Analyze.py
+++ Analyze.py
@@ -1,20 +1,21 @@
 # %%
 # Импорт библиотек, чтение и вывод фигуры данных
 import pandas as pd
-# import numpy as np
+import numpy as np
 
 df = pd.read_csv('train.csv')
 df.shape
 
 # %%
 # Удаление пустых колонок, дропаем дупликаты и смотрим, что изменилось
+df = df.replace([np.inf, -np.inf, np.nan], 0)
 df.dropna(how='all', axis=1, inplace=True)
 df.drop_duplicates()
 df.shape
 
 # %%
-# Удаляем все колонки, у которых наполненность менее 1%
-df = df.loc[:, ((df == 0) | (df.isnull())).mean() < 0.99]
+# Удаляем все колонки, у которых наполненность менее 0.1%
+df = df.loc[:, ((df == 0) | (df.isnull())).mean() < 0.999]
 df.shape
 # %%
 # смотрим сколько колонок какого типа
@@ -36,14 +37,27 @@ for i, j in dic.items(): text = text.replace(i, j)
 f.write(text)
 f.close()
 # %%
+# Переводим колонки Float64 в Int64, которые можем
+float_columns = df.select_dtypes(include=np.number).columns
+for col in float_columns:
+    temp = df[col].fillna(0)
+    if temp.apply(lambda x: x == int(x)).all():
+        df[col] = df[col].astype(int)
+# %%
 # Делаем то же самое, но с float64
 text = ""
 for col in list(df.select_dtypes(['float']).columns): text += f"{col}:\n{df[col].unique()}\n\n\n"
 f = open('list_of_unique_values_in_float_cols.txt', 'w', encoding='utf-8')
 
-dic = {'nan':'',"' ":'\n'," '":'',"'":'','[':'',']':'','  ':'',}
-for i, j in dic.items(): text = text.replace(i, j)
+# dic = {'nan':'',"' ":'\n'," '":'',"'":'','[':'',']':'','  ':'',}
+# for i, j in dic.items(): text = text.replace(i, j)
 
 f.write(text)
 f.close()
+# %%
+
+# %%
+df.to_csv("cleaned_data.csv", index=False)
+# %%
+df.info()
 # %%
